1eee948bd56a44e75830a7829ac08f4e60df2868
bisrat-walle <bisratwalle3@gmail.com>
2021-12-10 10:23:05 +0300

    adding files

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { PRODUCTS } from './product.object';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log("Product Created!")
+  }
+
+
+  productList = PRODUCTS;
+
+
+
+
+
+}
---
+++ src/app/product/product.object.ts
@@ -0,0 +1,14 @@
+export interface Product{
+    id: number;
+    name: string;
+    price: number;
+    imageUrl: string;
+    description: string;
+}
+
+export const PRODUCTS: Product[ ] = [
+    { id: 1, name: 'Nike', price: 8, imageUrl: 'assets/pro_img.jfif', description:"Color: red and Size: 40"},
+    { id: 1, name: 'PUMA', price: 80, imageUrl: 'assets/pro_img.jfif', description:"Color: red and Size: 40"},
+    { id: 1, name: 'Addidas', price: 70, imageUrl: 'assets/pro_img.jfif', description:"Color: red and Size: 40"}
+]
+
